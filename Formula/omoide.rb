78aa4dafb0ddcff9655f19581f332ac25f398344
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-06-13 21:44:01 +0900

    Add formula for omoide

---
+++ Formula/omoide.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Omoide < Formula
+  desc "🤖 Browse and clean tweets in CLI"
+  homepage ""
+  url "https://github.com/himkt/omoide/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "c0e7170f45481302df25ff2d7d12ad7ae7d88b0ca7a338ba53bee79203743bad"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test omoide`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+```
+brew tap himkt/himkt
+```
