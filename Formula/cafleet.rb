2c47ef44d18cd43577f64789e64573036cc563c8
himkt <himkt@klis.tsukuba.ac.jp>
2026-08-08 14:16:20 +0900

    chore: bump cafleet to 0.23.4

--- Formula/cafleet.rb
+++ Formula/cafleet.rb
@@ -5,18 +5,18 @@ class Cafleet < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/himkt/cafleet/releases/download/0.23.1/cafleet-v0.23.1-aarch64-apple-darwin.tar.gz"
-      sha256 "fa2a6e3c36b7ac55e9ee97cc755f2fc67f00bc6fe2f375f6b557f2d1b6ff6cfc"
+      url "https://github.com/himkt/cafleet/releases/download/0.23.4/cafleet-v0.23.4-aarch64-apple-darwin.tar.gz"
+      sha256 "47946c97359951dbd5345dd4259db2986186be9ef6559c4463686a0ee4d3ddb2"
     end
   end
   on_linux do
     on_arm do
-      url "https://github.com/himkt/cafleet/releases/download/0.23.1/cafleet-v0.23.1-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "dede0cb0511147e3b7b3be65df9e98dc78ae82f9bbd512f1f762cb343683c811"
+      url "https://github.com/himkt/cafleet/releases/download/0.23.4/cafleet-v0.23.4-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "40624d5906a1b1ef2bc25f17b1d563cc9997297852cd9a286b1a997615923d05"
     end
     on_intel do
-      url "https://github.com/himkt/cafleet/releases/download/0.23.1/cafleet-v0.23.1-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "8dd3500998412d2ad0e12631cc9b0dc6364f4fa0e2c43fbfebda25562a3bee61"
+      url "https://github.com/himkt/cafleet/releases/download/0.23.4/cafleet-v0.23.4-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "24eeebc15cd3c70bc14e64eda9a8c95cbd2e9a0f1f3623145cbd2949ab76ac47"
     end
   end
   def install
